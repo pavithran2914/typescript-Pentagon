580890a083642ce2ff8a99c4647a86c4e2f7808f
pavithran2914 <pavithranbala29@gmail.com>
2022-03-12 18:47:33 +0530

    1 file modified

--- pentagon.model.ts
+++ pentagon.model.ts
@@ -1,12 +1,19 @@
 import { Dot } from './dot.model';
 import { Coordinate } from './coordinate.model';
 
-export class Pentagon {
+interface IShapes<T> {
+  area(shape: T): T;
+}
+
+export class Pentagon implements IShapes<Pentagon> {
   dot: Dot;
   pen: Coordinate;
 
   constructor(dot: Dot, pen: Coordinate) {
     this.dot = dot;
     this.pen = pen;
+  }
+  area(shape: Pentagon): Pentagon {
+    throw new Error('Method not implemented.');
   }
 }
